59495faa1979e8b956af389bc1c894162722d6dd
Atharva Borade <atharvaborade568@gmail.com>
2025-07-21 14:39:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
-from flask import Flask,render_template,request
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
+quotes = []
 
-@app.route("/", methods=["GET","POST"])
+@app.route("/", methods=["GET", "POST"])
 def home():
     if request.method == "POST":
-      with open("file.txt","a") as f:
-         f.write(f"\nthe quote given by user is {request.form["Quote"]}, and this is written by {request.form["Author"]} \n")
+        with open("file.txt", "a") as f:
+            f.write(f"\nthe quote given by user is {request.form['Quote']}, and this is written by {request.form['Author']}\n")
 
-    return render_template("index.html")
-    
+    with open("file.txt", "r") as f:
+        quotes = f.readlines()
 
-app.run(debug = True)
+    return render_template("index.html", quotes=quotes)
+
+app.run(debug=True)
